6f29a63bbbbdd68e38dfb253064bfcef92cebae4
azalea <azalea@cs16091PC>
2016-07-14 15:19:12 +0900

    test

---
+++ src/org/aiwolf/GotKAgent/GotKRoleAssignPlayer.java
@@ -0,0 +1,13 @@
+package org.aiwolf.GotKAgent;
+
+import org.aiwolf.client.base.player.AbstractRoleAssignPlayer;
+
+public class GotKRoleAssignPlayer extends AbstractRoleAssignPlayer {
+
+	@Override
+	public String getName() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
